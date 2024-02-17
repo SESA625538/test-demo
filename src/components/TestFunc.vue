2a9515fdd9271276216e288a5e5175abdb4f34db
SESA625538 <carey.hu@se.com>
2024-02-18 01:08:20 +0800

    逆转世界案例测试

---
+++ src/components/TestFunc.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>{{ message }}</h1>
+    <button @click="btn">逆转世界</button>
+  </div>
+</template>
+ 
+<script>
+export default {
+  data() {
+    return {
+      message: "HELLO, WORLD",
+    };
+  },
+  methods: {
+    btn(){
+      this.message = this.message.split("").reverse().join("")
+    }
+  }
+};
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import TestFunc from '@/components/TestFunc'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
+   /*{
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    }*/
+    {
+      path: '/',
+      name: 'TestFunc',
+      component: TestFunc
     }
   ]
 })
