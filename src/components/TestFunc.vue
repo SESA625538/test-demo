ce233dd3492ce5cb1d13d78a875ad1926c35f948
SESA625538 <carey.hu@se.com>
2024-02-21 14:37:41 +0800

    v-bind,v-on,v-model区别测试

--- src/components/TestFunc.vue
+++ src/components/TestFunc.vue
@@ -1,20 +1,33 @@
 <template>
-  <div>
-    <h1>{{ message }}</h1>
-    <button @click="btn">逆转世界</button>
-  </div>
+
+    <div>
+        <!-- 属性绑定 -->
+        v-bind的完整:<input type="text" v-bind:value="bind"><br>
+        v-bind的简写:<input type="text" :value="bind">
+        <p>{{bind}}</p>
+        <!-- 双向绑定 -->
+        v-model的完整:<input type="text" v-model="model"><br>
+        v-model的简写:<input type="text" v-model="model">
+        <p>{{model}}</p>
+        <!-- 事件绑定 -->
+        <button v-on:click="von">我被点了{{on}}次</button>
+        <button @click="von">简写版被点了{{on}}次</button>
+    </div>    
+  
 </template>
  
 <script>
-export default {
+  export default {
   data() {
     return {
-      message: "HELLO, WORLD",
+      bind : '我是属性绑定',
+      model : '我是双向绑定', 
+      on : 0
     };
   },
   methods: {
-    btn(){
-      this.message = this.message.split("").reverse().join("")
+    von(){
+      this.on += 1
     }
   }
 };
