bf2acd12d128f5ce4cbfa6cbce4de6b1d9c12b9e
SESA625538 <carey.hu@se.com>
2024-02-21 15:17:05 +0800

    v-bind多值测试

--- src/components/TestFunc.vue
+++ src/components/TestFunc.vue
@@ -1,17 +1,11 @@
 <template>
-
     <div>
-        <!-- 属性绑定 -->
-        v-bind的完整:<input type="text" v-bind:value="bind"><br>
-        v-bind的简写:<input type="text" :value="bind">
-        <p>{{bind}}</p>
-        <!-- 双向绑定 -->
-        v-model的完整:<input type="text" v-model="model"><br>
-        v-model的简写:<input type="text" v-model="model">
-        <p>{{model}}</p>
-        <!-- 事件绑定 -->
-        <button v-on:click="von">我被点了{{on}}次</button>
-        <button @click="von">简写版被点了{{on}}次</button>
+        <!-- 使用数组形式进行多值绑定 -->
+        <p :class="[vforecolor, vbackcolor]">{{vdata}}</p>
+        <!-- 多值在Vue的data属性中已经进行绑定 -->
+        <p :class="vclass">{{vdata}}</p>
+        <!-- 根据真假渲染 -->
+        <p :class="{forecolor:vclass1, backcolor:vclass2}">{{vdata}}</p>
     </div>    
   
 </template>
@@ -20,15 +14,22 @@
   export default {
   data() {
     return {
-      bind : '我是属性绑定',
-      model : '我是双向绑定', 
-      on : 0
+      vdata:            "我修改了字体颜色",
+      vforecolor:       "forecolor",
+      vbackcolor:       "backcolor",
+      vclass:           'forecolor backcolor',
+      vclass1:          true,
+      vclass2:          false
     };
-  },
-  methods: {
-    von(){
-      this.on += 1
-    }
   }
 };
 </script>
+
+<style>
+    .forecolor{
+        color: burlywood;
+    }
+    .backcolor{
+        background: chartreuse;
+    }
+</style>
